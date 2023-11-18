93c91d679343e1705841954a7568ace70b2f2cb3
CbIPOPE3KA <cheesecutter@yandex.ru>
2023-11-18 14:09:15 +0500

    На всякий

--- script.js
+++ script.js
@@ -902,7 +902,7 @@ function emoteToSpeech() {
 
     // Обрабатываем текст с помощью регулярного выражения (здесь просто пример)
     let updatedEmoteText = emoteText.replace(
-      /(–\s(?:["«])?[А-Я](?:.+?)(?:(?:<\/span>)| – ))/g,
+      /(–\s(?:["«])?[А-Я](?:.+?)(?:[,.!?] –|<\/span>))/g,
       '<span class="speech">$1</span>'
     );
 
